0cffab2ab71ae795e472753e0e21c9336935f12b
Spencer <spencer_j_smith@outlook.com>
2025-10-16 20:22:21 -0600

    Count to 15 woohoo

--- app/src/main.c
+++ app/src/main.c
@@ -7,43 +7,34 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
-#include <BTN.h>
-#include <LED.h>
+#include "BTN.h"
+#include "LED.h"
 
 #define SLEEP_MS 1
 
-/*#define SW0_NODE DT_ALIAS(sw0)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
-static struct gpio_callback button_isr_data;
 
-void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
-  printk("ily\n");
-} */
 
 int main(void) {
   if (0 > BTN_init() || 0 > LED_init())
     return 0;
 
-/*  int ret;
+  int sum = 0;
 
-  if (!gpio_is_ready_dt(&button))
-    return 0;
-  
-  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-  if (0 > ret)
-    return 0;
-
-  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-  if (0 > ret)
-    return 0;
-  
-  gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
-  gpio_add_callback(button.port, &button_isr_data); */
 
   while(1) {
     if (BTN_check_clear_pressed(BTN0)) {
-      LED_toggle(LED0);
+      sum++;
+      if (16 == sum)
+        sum = 0;
+      
+      printk("%d\n", sum);
     }
+    
+    LED_set(LED0, sum&1);
+    LED_set(LED1, sum&2);
+    LED_set(LED2, sum&4);
+    LED_set(LED3, sum&8);
+
 
     k_msleep(SLEEP_MS);
   }
